2d0551e2bc48ab6d56dda0f99b6ea9db9be5d6b0
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2021-05-08 10:55:35 +0300

    Routing for the menuitems

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ export default class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} size={size} title={title} imageUrl={imageUrl} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
