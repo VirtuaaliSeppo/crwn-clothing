2458a7e03a2f80f07e955eb8c37be815b0333ba3
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2021-12-02 07:27:48 +0200

    Add stripe payment, new title, new favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JxriNBdhhUxyfMyDvxVI2pZVaJyk8YptpWUjsiB3NEuYOCvzqSMGGTmMAIg9WLkW4Nv3fIcGZHjmk2hCrLYdlCU00m3e2fPMI";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay now..."
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
